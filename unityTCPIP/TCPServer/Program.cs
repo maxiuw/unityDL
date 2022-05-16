0a22cca7556b458ac651c775287f4fce1481604f
maxiuw <maciejwozniak94@gmail.com>
2022-05-16 15:15:04 +0200

    adding ocs lib - udp based

--- unityTCPIP/TCPServer/Program.cs
+++ unityTCPIP/TCPServer/Program.cs
@@ -7,7 +7,7 @@ namespace TCPServer
     {
         static void Main(string[] args)
         {
-            Thread serverThread = new Thread(() => new Server("127.0.0.1" , 13000));
+            Thread serverThread = new Thread(() => new Server("127.0.0.1" , 6969));
             serverThread.Start();
             
             // server is going to be launch from here
